2957611dfcff6afac86ed92b251a1facff9894a4
sufyanmalik90 <shaikh.arman1239090@gmail.com>
2023-12-12 00:15:20 +0500

    working-recipesCard

--- src/app/components/RecipeCard.tsx
+++ src/app/components/RecipeCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Timer from "../../../public/Timer.svg";
 import Knife from "../../../public/ForkKnife.svg";
 
-const RecipeCard = ({ imageUrl, name, duration, categorie }: any) => {
+const RecipeCard = ({ imageUrl, name, duration, category }: any) => {
   return (
     <div className="max-w-sm rounded-[30px] bg-[#E7F9FD] mb-8 overflow-hidden">
       <div className="h-48 w-[400px] relative">
@@ -10,6 +10,7 @@ const RecipeCard = ({ imageUrl, name, duration, categorie }: any) => {
           className="w-full h-full object-fill"
           src={imageUrl}
           alt={name}
+          layout="fill"
         />
       </div>
       <div className="px-6 py-4">
@@ -18,7 +19,7 @@ const RecipeCard = ({ imageUrl, name, duration, categorie }: any) => {
           <Image className="w-5 h-5 object-fill" src={Timer} alt="timer" />
           <p className="text-sm font-medium font-['Inter']">{duration}</p>
           <Image className="w-5 h-5 object-fill" src={Knife} alt="Knife" />
-          <p className="text-sm font-medium font-['Inter']">{categorie}</p>
+          <p className="text-sm font-medium font-['Inter']">{category}</p>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,99 +17,71 @@ import meatImg  from "../../public/meatImg.png";
 import healthy  from "../../public/healthy.png";
 import TitleDescriptionImage from "./components/TitleDescriptionImage";
 import Feedback from "./components/Feedback";
+import recipesData from '../app/components/recipes.json'
 export default function Home() {
   return (
-   
-      <main className="px-4">
-        <Categories />
-        <div className="flex justify-around mx-4 flex-wrap">
-          <ImageCard
-            imageUrl={Cat}
-            title="Breakfast"
-            gradientColor="rgba(112, 130, 70, 0.10)"
-          />
-          <ImageCard
-            imageUrl={Cat2}
-            title="Vegan"
-            gradientColor="rgba(108, 198, 63, 0.10)"
-          />
-          <ImageCard
-            imageUrl={Cat3}
-            title="Meat"
-            gradientColor="rgba(204, 38, 27, 0.10)"
-          />
-          <ImageCard
-            imageUrl={Cat4}
-            title="Desert"
-            gradientColor="rgba(240, 158, 0, 0.10)"
-          />
-          <ImageCard
-            imageUrl={Cat5}
-            title="Lunch"
-            gradientColor="rgba(0, 0, 0, 0.05)"
-          />
-          <ImageCard
-            imageUrl={Cat6}
-            title="Chocolate"
-            gradientColor="rgba(0, 0, 0, 0.05)"
-          />
-        </div>
-        <Section
-          title="Simple and tasty recipes"
-          content="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim."
+    <main className="px-4">
+      <Categories />
+      <div className="flex justify-around mx-4 flex-wrap">
+        <ImageCard
+          imageUrl={Cat}
+          title="Breakfast"
+          gradientColor="rgba(112, 130, 70, 0.10)"
         />
-        <div className="flex flex-wrap justify-evenly">
-          <RecipeCard
-            imageUrl={RecipeCard1}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Snack"
-          />
-          <RecipeCard
-            imageUrl={Fish}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Fish"
-          />
-          <RecipeCard
-            imageUrl={Honey}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Breakfast"
-          />
-          <RecipeCard
-            imageUrl={healthy}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Healthy"
-          />
-          <RecipeCard
-            imageUrl={meatImg}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Meat"
-          />
-          <RecipeCard
-            imageUrl={Sweet}
-            name="Delicious Pasta"
-            duration="30 minutes"
-            categorie="Sweet"
-          />
-        </div>
-        <div className="flex h-[597px] w-full">
-          <TitleDescriptionImage
-            title="Everyone can be a chef in their own kitchen"
-            description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
-            imageUrl={Chef}
-            btn_text={'Learn More'}
-          />
-        </div>
-        <Section
-          title="Check out @foodieland on Instagram"
-          content="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim."
+        <ImageCard
+          imageUrl={Cat2}
+          title="Vegan"
+          gradientColor="rgba(108, 198, 63, 0.10)"
         />
-        <Feedback />
-      </main>
-   
+        <ImageCard
+          imageUrl={Cat3}
+          title="Meat"
+          gradientColor="rgba(204, 38, 27, 0.10)"
+        />
+        <ImageCard
+          imageUrl={Cat4}
+          title="Desert"
+          gradientColor="rgba(240, 158, 0, 0.10)"
+        />
+        <ImageCard
+          imageUrl={Cat5}
+          title="Lunch"
+          gradientColor="rgba(0, 0, 0, 0.05)"
+        />
+        <ImageCard
+          imageUrl={Cat6}
+          title="Chocolate"
+          gradientColor="rgba(0, 0, 0, 0.05)"
+        />
+      </div>
+      <Section
+        title="Simple and tasty recipes"
+        content="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim."
+      />
+      <div className="flex flex-wrap justify-evenly">
+      {recipesData.map((recipe, index) => (
+          <RecipeCard
+            key={index}
+            imageUrl={`/${recipe.image}`}
+            name={recipe.name}
+            duration={recipe.duration}
+            category={recipe.category}
+          />
+        ))}
+      </div>
+      <div className="flex h-[597px] w-full">
+        <TitleDescriptionImage
+          title="Everyone can be a chef in their own kitchen"
+          description="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim "
+          imageUrl={Chef}
+          btn_text={"Learn More"}
+        />
+      </div>
+      <Section
+        title="Check out @foodieland on Instagram"
+        content="Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim."
+      />
+      <Feedback />
+    </main>
   );
 }
