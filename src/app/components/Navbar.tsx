ea6fd450ad06866e34035ca2ebbe8bb28d5af2d0
sufyanmalik90 <shaikh.arman1239090@gmail.com>
2023-12-15 00:51:09 +0500

    done-sidebar-nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,21 +4,58 @@ import Image from 'next/image';
 import Logo from '../../../public/vercel.svg';
 import '@fortawesome/fontawesome-free/css/all.css'; 
 import { Menu } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const Navbar = () => {
   return (
     <nav className="p-4 justify-around flex border-b-2 items-center">
       <div className='flex md:hidden'>
-      <Menu/>
+      
+      <Sheet>
+  <SheetTrigger><Menu/></SheetTrigger>
+  <SheetContent side={'left'}>
+  <ul className="flex flex-col gap-y-5 text-black text-[16px]">
+          <li>
+            <Link href='/'>
+            Home
+            </Link>
+            </li>
+          <li>
+          <Link href='/recipes'>
+            Recipes</Link>
+            </li>
+          <li>
+          <Link href='/blog'>Blog</Link></li>
+          <li>
+          <Link href='/contact'>Contact</Link></li>
+          <li><Link href='/about'>About us</Link></li>
+        </ul>
+    {/* <SheetHeader>
+      <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader> */}
+  </SheetContent>
+</Sheet>
 
       </div>
         <div className="">
           <Link href="/" >
-          <Image  src={Logo} width={100} height={100} alt='logo' className='h-14 w-14'/>
+          <Image  src={Logo} width={100} height={100} alt='logo' className='h-14 w-14 hidden md:flex'/>
           </Link>
         </div>
       <div>
-        <ul className="flex gap-x-5 text-black text-[16px]">
+        <ul className="hidden md:flex gap-x-5 text-black text-[16px]">
           <li>
             <Link href='/'>
             Home
